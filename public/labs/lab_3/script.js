2a29d991e7fbe5b34c2c841baabed419268d6c34
ktsai99 <78124800+ktsai99@users.noreply.github.com>
2021-02-16 00:05:21 -0500

    Update script.js

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,7 +1,7 @@
 let i = 1;
     for(let li of carousel.querySelectorAll('li')) {
       li.style.position = 'relative';
-      li.insertAdjacentHTML('beforeend', <span style="position:absolute;left:0;top:0">${i}</span>);
+      li.insertAdjacentHTML('beforeend', '<span style="position:absolute;left:0;top:0">${i}</span>);
       i++;
     }
 
